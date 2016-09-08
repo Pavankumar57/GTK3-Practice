1e490f819c50499445f8ab29f068b812d5a98dac
= <=>
2016-09-08 19:01:13 +0530

    window and button commit

---
+++ button.py
@@ -0,0 +1,16 @@
+import gi
+gi.require_version('Gtk','3.0')
+from gi.repository import Gtk
+
+class HelloWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self,title = "Hello")
+        button = Gtk.Button(label = "Click Here")
+        button.connect("clicked",self.onClick,"pavan")
+        self.add(button)
+    def onClick(self, widget,string):
+        print("hello World "+ string)
+win = HelloWindow()
+win.connect("delete-event",Gtk.main_quit)
+win.show_all()
+Gtk.main()
